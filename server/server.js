e85b77121c643a1ab628dbd980209d54b9386aaf
Harsh Prajapati <harshprajapati8347@gmail.com>
2025-07-05 16:14:43 +0530

    feat - dashboard page - backup

--- server/server.js
+++ server/server.js
@@ -3,31 +3,33 @@ import app from "./app.js";
 import connectDatabase from "./db/Database.js";
 import dotenv from "dotenv";
 
-// Handling uncaught Exception
+// Uncaught Exception
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
   console.log(`shutting down the server for handling uncaught exception`);
 });
 
-// config
+// Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   dotenv.config({ path: "./.env" });
 }
 
-// connect database
+// DB Connection
 connectDatabase();
 
+// Server
 const server = app.listen(process.env.PORT || 8080, () => {
   console.log(
     `Server is running on http://localhost:${process.env.PORT || 8080}`
   );
 });
 
+// Routes
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// Handling unhandled Promise Rejection
+// Unhandled Rejection
 process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
   console.log(
